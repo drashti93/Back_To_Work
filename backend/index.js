af66a21844c3c1900fd75f03256c6a94f0dcd4ba
Drusti Thakkar <drustihitesh.thakkar@sjsu.edu>
2019-08-11 12:12:07 -0700

    Mongoose and models

---
+++ backend/index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cors = require('cors');
+var MongoDBStore = require('connect-mongodb-session')(session);
+
+var mongoose = require('../backend/src/resources/mongoose')
+
+app.use(
+  cors({
+      origin: `http://localhost:3000`,
+      credentials: true
+  })
+);
+app.use('/static',express.static(__dirname + 'public'));
+
+var sessionStore = new MongoDBStore({
+  uri: `mongodb://root:root@cluster0-shard-00-00-ggwql.mongodb.net:27017,cluster0-shard-00-01-ggwql.mongodb.net:27017,cluster0-shard-00-02-ggwql.mongodb.net:27017/root?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`,
+  collection: 'site_sessions'
+});
+
+app.use(session({
+  secret: "sjlabssecret",
+  resave: false,
+  saveUninitialized: false,
+  duration: 600000000000 * 60 * 1000,
+  activeDuration: 6 * 60 * 60 * 1000,
+    cookie : {
+      maxAge: 1000* 60 * 60 *24 * 365,
+      expires : 3600000 * 24 * 60
+  },
+  store: sessionStore
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static',express.static('public'));
+
+app.use(function(req, res, next) {
+  res.setHeader(
+      "Access-Control-Allow-Origin",
+      `http://localhost:3000`
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, XMLHttpRequest"
+  );
+  res.setHeader("Cache-Control", "no-cache");
+  next();
+});
+
+app.listen(8000);
+console.log("Server listening on port 8000");
--- backend/src/resources/mongoose.js
+++ backend/src/resources/mongoose.js
@@ -1,22 +1,10 @@
-var mongoose = require("mongoose");
-
+var mongoose = require('mongoose');
+var uri = `mongodb://root:root@cluster0-shard-00-00-ggwql.mongodb.net:27017,cluster0-shard-00-01-ggwql.mongodb.net:27017,cluster0-shard-00-02-ggwql.mongodb.net:27017/root?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`
 mongoose.Promise = global.Promise;
-mongoose
-	.connect(
-		"mongodb://root:root@cluster0-shard-00-00-ggwql.mongodb.net:27017,cluster0-shard-00-01-ggwql.mongodb.net:27017,cluster0-shard-00-02-ggwql.mongodb.net:27017/root?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",
-		{
-			useCreateIndex: true,
-			useNewUrlParser: true,
-			poolSize: 10
-		}
-	)
-	.then(
-		() => {
-			console.log(`Sucessfully Connected to MogoDB`);
-		},
-		err => {
-			console.log(`Error Connecting to MogoDB: ${err}`);
-		}
-	);
+console.log(uri);
+mongoose.connect(uri).then(
+    () => { console.log("Connected to MongoDB") },
+    err => { console.log("Did not connect", err) }
+  );
 
-module.exports = { mongoose };
+module.exports = {mongoose};
